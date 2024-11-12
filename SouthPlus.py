9a329b2a7a00e32e6401d03ff3dcacdae9dd925b
ayyayyayy2002 <joshualovejoy@outlook.com>
2024-11-12 23:05:10 +0800

    aaaaaaaaaa

---
+++ SouthPlus.py
@@ -0,0 +1,84 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from selenium.common import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import os
+
+
+
+
+
+
+
+
+#chrome_binary_path = os.environ['CHROME']
+#chrome_driver_path = = os.environ['DRIVER']
+SOUTHPLUS = os.environ['SOUTHPLUS']
+UA = os.environ['UA']
+cookies = {}
+for cookie in SOUTHPLUS.split('; '):
+    name, value = cookie.split('=', 1)  # 只分割第一个等号
+    cookies[name] = value
+
+
+
+
+#base_dir = os.path.dirname(os.path.abspath(__file__))
+#chrome_binary_path = os.path.join(base_dir, '附加文件', 'chrome-win', 'chrome.exe')
+#chrome_driver_path = os.path.join(base_dir, '附加文件', '运行数据','chromedriver.exe')
+
+
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-blink-features=AutomationControlled")
+#options.binary_location = chrome_binary_path  # 指定 Chrome 浏览器的可执行文件路径
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-sync")
+options.add_argument(f'--user-agent={UA}')
+options.add_argument("disable-cache")#禁用缓存
+options.add_argument("--headless")
+#service = Service(executable_path=chrome_driver_path)
+driver = webdriver.Chrome(options=options)  # 启动 Chrome 浏览器
+#driver = webdriver.Chrome(service=service,options=options)  # 启动 Chrome 浏览器
+driver.set_window_size(1000, 700)  # 设置浏览器窗口大小（宽度, 高度）
+#driver.set_window_position(-850, 775)  # 设置浏览器窗口位置（x, y）
+#driver.set_window_position(-850, 1355)
+driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+
+driver.get("https://www.south-plus.net/")
+for name, value in cookies.items():
+    driver.add_cookie({'name': name, 'value': value})
+
+try:
+    driver.get("https://www.south-plus.net/plugin.php?H_name-tasks.html")
+    daily = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="p_15"]/a/img'))
+    )
+    daily.click()
+    # 创建一个名为a.txt的空文件
+    with open("a.txt", "a", encoding='utf-8') as file:
+        file.write("\n  ✔日常任务领取！")
+
+except TimeoutException:
+    with open("a.txt", "a", encoding='utf-8') as file:
+        file.write("\n  ❗日常领取失败！")
+
+
+try:
+    driver.get("https://www.south-plus.net/plugin.php?H_name-tasks-actions-newtasks.html.html")
+    element = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="both_15"]/a/img'))
+    )
+    element.click()
+    with open("a.txt", "a", encoding='utf-8') as file:
+        file.write("\n  ✔日常任务完成！")
+
+except TimeoutException:
+    with open("a.txt", "a", encoding='utf-8') as file:
+        file.write("\n  ❗日常任务失败！")
+
+
+
+driver.quit()
+exit(0)
---
+++ main.py
@@ -0,0 +1,113 @@
+import subprocess
+import requests
+import os
+
+
+
+
+
+
+
+
+
+
+
+
+
+with open("a.txt", "w", encoding='utf-8') as file:
+    pass
+UA = os.environ.get('UA')
+BOTTOKEN = os.environ.get('BOTTOKEN')
+USERID = os.environ.get('USERID')
+SOUTHPLUS = os.environ.get('SOUTHPLUS')
+ACGFUN = os.environ.get('ACGFUN')
+
+# 检查 UA 是否存在，并打印
+if UA:
+
+    if SOUTHPLUS:
+        with open("a.txt", "a", encoding='utf-8') as file:
+            file.write("\nSouthplus签到结果：")
+        try:
+            # 运行 SouthPlus.py 脚本
+            result = subprocess.run(['python', 'SouthPlus.py'], check=True, capture_output=True, text=True)
+            print(result.stdout)  # 如果成功，打印标准输出
+        except Exception as e:
+            print(e)
+            with open("a.txt", "a", encoding='utf-8') as file:
+                file.write("\n  😅SouthPlus签到出错")
+    else:
+        with open("a.txt", "a", encoding='utf-8') as file:
+            file.write("\n  😢不进行Southplus签到")
+
+
+
+
+    if ACGFUN:
+        with open("a.txt", "a", encoding='utf-8') as file:
+            file.write("\nAcgFun签到结果：")
+        try:
+            # 运行 SouthPlus.py 脚本
+            result = subprocess.run(['python', 'AcgFun.py'], check=True, capture_output=False, text=True)
+            print(result.stdout)  # 如果成功，打印标准输出
+        except Exception as e:
+            print(e)
+            with open("a.txt", "a", encoding='utf-8') as file:
+                file.write("\n  😅AcgFun签到出错")
+
+    else:
+        with open("a.txt", "a", encoding='utf-8') as file:
+            file.write("\n  😢不进行AcgFun签到")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+else:
+    with open("a.txt", "a", encoding='utf-8') as file:
+        file.write("❗未设置UA，脚本拒绝执行")
+
+
+
+
+
+
+
+
+if USERID:
+    with open('a.txt', 'r', encoding='utf-8') as file:
+        content = file.read()  # 读取文件内容
+
+    # 发送消息到 Telegram
+    url = f'https://api.telegram.org/bot{BOTTOKEN}/sendMessage'
+    payload = {
+        'chat_id': USERID,
+        'text': content,
+    }
+
+    response = requests.post(url, data=payload)
+
+else:
+    exit(0)
