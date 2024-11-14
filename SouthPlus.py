e5e7e07775798abfc2df9a9113fa5cb129fb822d
Ayyayyayy2002 <joshualovejoy@outlook.com>
2024-11-14 08:55:36 +0800

    Update SouthPlus.py

--- SouthPlus.py
+++ SouthPlus.py
@@ -3,6 +3,17 @@ from selenium import webdriver
 import undetected_chromedriver as uc
 import os
 
+import subprocess
+
+# 获取Chrome浏览器的版本号
+chrome_version = subprocess.check_output("google-chrome --version", shell=True).decode('utf-8')
+print(chrome_version)
+
+import subprocess
+
+# 获取Chrome浏览器的版本号
+chrome_version = subprocess.check_output("chromium-browser --version", shell=True).decode('utf-8')
+print(chrome_version)
 
 
 
